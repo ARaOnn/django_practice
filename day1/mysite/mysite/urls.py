a01463221dee9951332845ec217ef32f956f68b6
JHY <yun01061991@naver.com>
2023-06-17 12:13:28 +0900

    day1_1

--- day1/mysite/mysite/urls.py
+++ day1/mysite/mysite/urls.py
@@ -16,7 +16,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse("<h1>hello</h1>")
+
+def index(request):
+    html = """<!doctype html>
+    <html>
+        <head>
+            <title>Django</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">Django</a></h1>
+            <ol>
+                <li><a href="/chapter01/">Setting & Deploy</a></li>
+                <li><a href="/chapter02/">Routing & View</a></li>
+            </ol>
+            <h2>Django</h2>
+            <p><a href="https://www.djangoproject.com/" target="_blank">Django</a>는
+            Python으로 작성된 오픈 소스 웹 프레임워크로, 빠르고 쉬운 웹 개발을 가능하게 합니다.
+            </p>
+        </body>
+    </html>"""
+    return HttpResponse(html)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('hello/', hello),
+    path('', index),
 ]
+
