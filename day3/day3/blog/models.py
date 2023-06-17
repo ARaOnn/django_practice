2197dfbd64dd7590254d6aea5a36a84e9d3d39b0
JHY <yun01061991@naver.com>
2023-06-18 06:51:32 +0900

    day3_2

--- day3/day3/blog/models.py
+++ day3/day3/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length= 100)
+	content = models.TextField()
+	created_at = models.DateField(auto_now_add= True)
+	published_at = models.DateField(null= True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
--- day3/day3/blog/views.py
+++ day3/day3/blog/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
-posts = {
-		'01' : {'title' : 'Template', 'content' : 'Template is ...'},
-		'02' : {'title' : 'orm' , 'content' : 'orm is ...'}
-}
+# posts = {
+# 		'01' : {'title' : 'Template', 'content' : 'Template is ...'},
+# 		'02' : {'title' : 'orm' , 'content' : 'orm is ...'}
+# }
 
-def home(req):    
+def home(req):   
+    posts = models.Post.objects.all() 
     return render(req, 'blog/index.html', {
         'post_list' : posts,
-        'title' : 'Django', 
-        'content' : 'Django is ...'})
+        'post' : {'title' : 'Django', 
+        'content' : 'Django is ...'}
+        })
     
 def post_list(req, id):
-    
+    posts = models.Post.objects.all()
+    post = models.Post.objects.get(id=id)
     return render(req, 'blog/index.html', {
-        'post_list' : posts, 
-        **posts.get(id) })
+        'post_list': posts, 
+        'post': post})
